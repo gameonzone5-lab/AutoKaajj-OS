dcf7863958cf0cc13dd17b961c5309b5500edd20
root <root@localhost.localdomain>
2026-06-21 19:25:40 +0000

    Permanent AutoKaaj branding applied via fix_welcome.sh

--- fix_welcome.sh
+++ fix_welcome.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sed -i 's/Welcome to Ubuntu in UserLAnd!/Welcome to AutoKaaj AI Environment!/g' /etc/motd
-sed -i 's/Welcome to [a-zA-Z]* in UserLAnd!/Welcome to AutoKaaj AI Environment!/g' /home/*/.bashrc
-sed -i 's/UserLAnd/AutoKaaj AI/g' /home/*/.bashrc
+# AutoKaaj Permanent Rebranding Script
+rm -f /etc/motd
+echo "Welcome to AutoKaaj AI Environment!" > /etc/motd
+sed -i 's/UserLAnd/AutoKaaj AI/g' /home/cm/.bashrc
+sed -i 's/Welcome to Ubuntu in UserLAnd!//g' /home/cm/.bashrc
+clear
