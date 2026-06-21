d819a75d56bd7203f4b2c8d6842a685de25ff206
root <root@localhost.localdomain>
2026-06-21 20:14:20 +0000

    Ultimate branding fix

--- fix_welcome.sh
+++ fix_welcome.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# AutoKaaj Permanent Rebranding Script
+# একদম ফিক্সড ব্র্যান্ডিং
+clear
+# সব পুরোনো মেসেজ ফাইল মুছে ফেলা
 rm -f /etc/motd
+rm -f /etc/issue
+# নতুন ব্র্যান্ড মেসেজ সেট করা
 echo "Welcome to AutoKaaj AI Environment!" > /etc/motd
-sed -i 's/UserLAnd/AutoKaaj AI/g' /home/cm/.bashrc
-sed -i 's/Welcome to Ubuntu in UserLAnd!//g' /home/cm/.bashrc
+# ইউজারল্যান্ড সংক্রান্ত সব টেক্সট মুছে ফেলা
+sed -i '/UserLAnd/d' /home/cm/.bashrc 2>/dev/null
+sed -i '/Welcome to Ubuntu/d' /home/cm/.bashrc 2>/dev/null
+# টার্মিনাল প্রম্পট সেট করা
+export PS1="AutoKaaj:~\$ "
 clear
